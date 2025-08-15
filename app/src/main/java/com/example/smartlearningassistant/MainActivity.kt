f4508e1d6c4a42307387eecabd92c07c209489d0
devkeshav05 <kmishra_be23@thapar.edu>
2025-08-15 17:46:56 +0530

    chore: initial project setup

---
+++ app/src/main/java/com/example/smartlearningassistant/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartlearningassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Links to your XML layout
+    }
+}
